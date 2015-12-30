2623a3e213acebc067a309fe56f7225434d588be
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2015-12-30 17:42:31 -0300

    [ADD] corrigindo a build

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@
 from distutils.core import setup
 setup(
     name='django_model_documentation',
-    packages=['django_model_documentation'], # this must be the same as the name above
-    version='0.1',
+    packages=['django_model_documentation',
+              'django_model_documentation.management',
+              'django_model_documentation.management.commands'],
+    version='0.2',
     description='Django Application for output a documentation of apps models',
     author='Kelson da Costa Medeiros',
     author_email='kelsoncm@gmail.com',
-    url='https://github.com/kelsoncm/django_model_documentation', # use the URL to the github repo
-    download_url='https://github.com/kelsoncm/django_model_documentation/tarball/0.1', # I'll explain this in a second
-    keywords=['django', 'model', 'documentation',], # arbitrary keywords
-    classifiers=[],
+    url='https://github.com/kelsoncm/django_model_documentation', 
+    download_url='https://github.com/kelsoncm/django_model_documentation/tarball/0.2',
+    keywords=['django', 'model', 'documentation', ],
+    classifiers=[]
 )

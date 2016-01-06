71ce161fb479d52aa27270938a38e482042e5523
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2016-01-06 18:07:35 -0300

    [BUG] Correction this tag path

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Kelson da Costa Medeiros',
     author_email='kelsoncm@gmail.com',
     url='https://github.com/kelsoncm/django_model_documentation', 
-    download_url='https://github.com/kelsoncm/django_model_documentation/tarball/0.3',
+    download_url='https://github.com/kelsoncm/django_model_documentation/releases/tag/0.3',
     keywords=['django', 'model', 'documentation', ],
     classifiers=[]
 )
